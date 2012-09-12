d97dbbc0a508dded36309ffdeb3bb7fec8536a30
Andras Jeszenszky <jsi@jss.hu>
2012-09-12 07:08:40 +0200

    valtozok es fuggveny nevek egyertelmusitese

--- ami/originate.c
+++ ami/originate.c
@@ -19,7 +19,7 @@
 
 // 6 byte-os random hexa stringet masol az ami->uuid bufferbe
 // TODO: egy rendes, unique ID-t visszaado fuggvenyt irni ehelyett a random vacak helyett
-static void generate_uuid (char *dst, size_t size) {
+static void generate_oid (char *dst, size_t size) {
 	struct timeval tv;
 	int num;
 	char tmp[16];
@@ -36,59 +36,55 @@ static void dumpi (ami_event_t *event) {
 	ami_event_dump(event);
 }
 
-static void got_uniqueid (ami_ori_t *ami_ori, char *uniqueid) {
-	ami_t *ami = ami_ori->ami;
-	//~ strncpy(ami_ori->uniqueid, uniqueid, sizeof(ami_ori->uniqueid) - 1);
+static void got_uniqueid (ori_t *ori, char *uniqueid) {
+	ami_t *ami = ori->ami;
+	//~ strncpy(ori->uniqueid, uniqueid, sizeof(ori->uniqueid) - 1);
 	ami_event_register(ami, dumpi, NULL, "Uniqueid: %s", uniqueid);
 }
 
 static void event_gotuuid_cb (ami_event_t *event) {
-	ami_ori_t *ami_ori = (ami_ori_t*)event->userdata;
+	ori_t *ori = (ori_t*)event->userdata;
 	char *uniqueid = ami_getvar(event, "Uniqueid");
 
-	printf("Megvan az uniqueid ==== %s\n", uniqueid);
-	got_uniqueid(ami_ori, uniqueid);
+	con_debug("got UniqueID=%s via VarSet method", uniqueid);
+	got_uniqueid(ori, uniqueid);
 }
 
 static void response_originate (ami_event_t *event) {
-	ami_ori_t *ami_ori = (ami_ori_t*)event->userdata;
-	ami_t *ami = ami_ori->ami;
+	ori_t *ori = (ori_t*)event->userdata;
+	ami_t *ami = ori->ami;
 
 	ami_event_dump(event);
 
 	char *uniqueid = ami_getvar(event, "Uniqueid");
-printf("*** ORIGINATE RESPONSE\n");
 	if (strlen(uniqueid)) {
-		printf("RESPONSE szinten megvan az uniqueid ==== %s\n", uniqueid);
-		got_uniqueid(ami_ori, uniqueid);
-		ami_event_unregister(ami, ami_ori->event_varsetuuid);
-		ami_ori->event_varsetuuid = NULL;
+		con_debug("got UniqueID=%s via Originate Response method", uniqueid);
+		got_uniqueid(ori, uniqueid);
+		ami_event_unregister(ami, ori->event_varset_uuid);
+		ori->event_varset_uuid = NULL;
 	}
 }
 
 // megrendelt AMI eventek lemondása
-void cleanup_events (ami_ori_t *ami_ori) {
-	ami_t *ami = ami_ori->ami;
+void cleanup_events (ori_t *ori) {
+	ami_t *ami = ori->ami;
 
-	ami_event_unregister(ami, ami_ori->event_originateresponse);
-	ami_ori->event_originateresponse = NULL;
+	ami_event_unregister(ami, ori->event_varset_uuid);
+	ori->event_varset_uuid = NULL;
 
-	ami_event_unregister(ami, ami_ori->event_varsetuuid);
-	ami_ori->event_varsetuuid = NULL;
-
-	ami_event_unregister(ami, ami_ori->action_originate);
-	ami_ori->action_originate = NULL;
+	ami_event_unregister(ami, ori->action_originate);
+	ori->action_originate = NULL;
 }
 
-ami_ori_t *ami_originate (ami_t *ami, const char *fmt, ...) {
-	ami_ori_t *ami_ori = malloc(sizeof(*ami_ori));
-	if (ami_ori == NULL) {
+ori_t *ami_originate (ami_t *ami, const char *fmt, ...) {
+	ori_t *ori = malloc(sizeof(*ori));
+	if (ori == NULL) {
 		con_debug("ami_originate() returned NULL");
 		return NULL;
 	}
-	bzero(ami_ori, sizeof(*ami_ori)); // minden NULL
+	bzero(ori, sizeof(*ori)); // minden NULL
 
-	ami_ori->ami = ami;
+	ori->ami = ami;
 
 	char buf[AMI_BUFSIZ];
 	va_list ap;
@@ -98,28 +94,28 @@ ami_ori_t *ami_originate (ami_t *ami, const char *fmt, ...) {
 	buf[AMI_BUFSIZ-1] = '\0'; // védelem
 
 	// AMI UUID
-	generate_uuid(ami_ori->uuid, sizeof(ami_ori->uuid));
+	generate_oid(ori->oid, sizeof(ori->oid));
 
-	ami_ori->event_varsetuuid = ami_event_register(ami_ori->ami, event_gotuuid_cb, ami_ori,
+	ori->event_varset_uuid = ami_event_register(ori->ami, event_gotuuid_cb, ori,
 		"Event: VarSet\n"
 		"Variable: ami_originate_id\n"
 		"Value: %s\n"
-		, ami_ori->uuid
+		, ori->oid
 	);
 
-	con_debug("sending originate with id %s", ami_ori->uuid);
-	ami_ori->action_originate = ami_action(ami, response_originate, ami_ori,
+	con_debug("sending originate with id %s", ori->oid);
+	ori->action_originate = ami_action(ami, response_originate, ori,
 		"Action: Originate\n"
 		"Async: 1\n"
 		"Variable: ami_originate_id=%s\n"
 		"%s"
-		, ami_ori->uuid, buf
+		, ori->oid, buf
 	);
 
-	return ami_ori;
+	return ori;
 }
 
-void ami_originate_destroy (ami_ori_t *ami_ori) {
+void ami_originate_destroy (ori_t *ori) {
 
 }
 
--- ami/originate.h
+++ ami/originate.h
@@ -3,17 +3,16 @@
 
 #include "ami.h"
 
-typedef struct ami_ori_t {
+typedef struct ori_t {
 	ami_t *ami;
-	char uuid[16];                                // ami_ori objektum saját egyedi azonosítója ActionID-hez
+	char oid[16];                                 // ori objektum saját egyedi azonosítója VarSet-hez
 	char uniqueid[32];                            // Asterisk híváshoz rendelt Uniqueid
 	ami_event_list_t *action_originate;
-	ami_event_list_t *event_originateresponse;
-	ami_event_list_t *event_varsetuuid;
-} ami_ori_t;
+	ami_event_list_t *event_varset_uuid;
+} ori_t;
 
-ami_ori_t *ami_originate (ami_t *ami, const char *fmt, ...);
+ori_t *ami_originate (ami_t *ami, const char *fmt, ...);
 
-void ami_originate_free (ami_ori_t *ami_ori);
+void ami_originate_free (ori_t *ori);
 
 #endif // #ifndef ORIGINATE_H_INCLUDED

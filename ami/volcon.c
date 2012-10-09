ed4d3cb8053d44a5e6bc5e7a4f6959d0275c1e86
Andras Jeszenszky <jsi@jss.hu>
2012-10-10 01:57:14 +0200

    kod csinositas

--- ami/volcon.c
+++ ami/volcon.c
@@ -9,7 +9,7 @@
 #include "ami.h"
 #include "debug.h"
 
-#define CON_DEBUG
+//~ #define CON_DEBUG
 #include "logger.h"
 
 ev_timer timer;
@@ -28,12 +28,12 @@ enum {
 void event_disconnect (ami_event_t *event) {
 	ami_t *ami = event->ami;
 	printf("* %s %s(%s:%s): %s\n",
-		(ami->authenticated) ? "Disconnected from" : "Can't connect to",
+		(!strcmp(ami_getvar(event, "WasAuthenticated"), "1")) ? "Disconnected from" : "Can't connect to",
 		ami_getvar(event, "Host"),
 		ami_getvar(event, "IP"),
 		ami_getvar(event, "Port"),
 		ami_getvar(event, "Reason"));
-	exit(0);
+	ami_connect_delayed(ami, 3000);
 }
 
 void event_connect (ami_event_t *event) {
@@ -132,6 +132,8 @@ void volume_reel (int vol_to, int steps, int fade_time) {
 
 	int vol_from = snd_getvolume();
 
+	printf("* Reeling volume %d%% -> %d%%\n", vol_from, vol_to);
+
 	// ha a jelenlegi és a beállítani kívánt hangerő ugyan az, akkor return
 	if (vol_to == vol_from)
 		return;
@@ -139,16 +141,20 @@ void volume_reel (int vol_to, int steps, int fade_time) {
 	int diff = abs(vol_from - vol_to);
 	float uni = (float)diff / (float)steps;
 
-debi(vol_from); debi(vol_to); debi(diff); debf(uni);
+//~ debi(vol_from); debi(vol_to); debi(diff); debf(uni);
 	int udelay = fade_time * 1000 / steps;
 	for (i = 0; i < steps; i++) {
 		int ch = (uni * i) * ((vol_from < vol_to) ? 1 : -1);
 		int destvol = vol_from + ch;
-		debi(ch); debi(destvol);
+//~ debi(ch); debi(destvol);
 		snd_setvolume(destvol);
 		usleep(udelay);
-		// ha az usleep() kozben egy masik program modositotta a hangerot,
-		// akkor azonnal visszaterunk
+
+		/* ha az usleep() kozben egy masik program modositotta a hangerot,
+		akkor azonnal visszaterunk. Tehat, ha a volume_reel() epp emeli a
+		hangerot, de a user egy masik hangeroszabalyzon hallkit egy kicsit,
+		akkor a volume_reel() azonnal visszalep es nem eroszakoskodik a
+		userrel. */
 		if (destvol != snd_getvolume())
 			return;
 	}
@@ -164,14 +170,20 @@ static void event_extensionstatus (ami_event_t *event) {
 	if (strcmp(exten, CONFIG_EXTEN))
 		return;
 
-	//~ con_debug("-- Exten: %s Status = %d", exten, status);
+	int current_volume = snd_getvolume();
 
 	switch (status) {
 		// Kagyló letesz, hangerő felhangosít
 		case 0: // Down
 			if (volume_state == HIGH) {
 				volume_state = LOW;
+
 				volume_low = snd_getvolume();
+
+				// ha a jelenlegi hangerő hangosabb, mint ahová felhangosítanánk a hangerőt
+				if (current_volume >= volume_high)
+					return;
+
 				if (volume_high >= 0)
 					volume_reel(volume_high, 20, 3000);
 			}
@@ -182,7 +194,14 @@ static void event_extensionstatus (ami_event_t *event) {
 		case 8: // Ring (?)
 			if (volume_state == LOW) {
 				volume_state = HIGH;
-				volume_high = snd_getvolume();
+
+				volume_high = current_volume;
+
+				// ha a jelenlegi hangerő hallkabb, mint ahová lehallkítanánk a hangerőt
+				if (current_volume <= volume_low)
+					return;
+
+
 				if (volume_low >= 0)
 					volume_reel(volume_low, 10, 1000);
 			}
@@ -198,24 +217,32 @@ int main (int argc, char *argv[]) {
 	}
 
 	if (argc < 2) {
-		printf("JSS AMI logger (c) 2012 JSS&Hayer - http://libamievent.jss.hu\n"
+		printf("JSS VolCon for Asterisk (c) 2012 JSS&Hayer - http://libamievent.jss.hu\n"
 		       "usage: %s <host>\n", argv[0]);
 		exit(1);
 	}
 
 	host = argv[1];
 
+	printf("* ALSA card name: %s\n", CONFIG_CARD);
+	printf("* Mixer controller name: %s\n", CONFIG_SELEM);
+	printf("* Watching extension %s ...\n", CONFIG_EXTEN);
+
+	volume_state = LOW; // feltételezzük, hogy indításnál a kagyló le van téve
+	volume_high = snd_getvolume(); // ezért a mostani hangerőt, mint hangos (high) hangerőt memorizáljuk
+	if (volume_high < 0) {
+		printf("ERROR fetching current volume...\n");
+		return 1;
+	} else {
+		printf("* Current volume: %d%%\n", volume_high);
+	}
+
 	printf("* Connecting to %s ...\n", host);
 	ami_credentials(ami, "jsi", "pwd", host, "5038");
-
 	ami_event_register(ami, event_disconnect, NULL, "Disconnect");
 	ami_event_register(ami, event_connect, NULL, "Connect");
 	ami_event_register(ami, event_extensionstatus, NULL, "Event: ExtensionStatus");
-
 	ami_connect(ami);
-
-	volume_state = LOW; // feltételezzük, hogy indításnál a kagyló le van téve
-	volume_high = snd_getvolume(); // feltételezzük, hogy a zene is hangos:)
 
 	ev_loop(EV_DEFAULT, 0);
 
